51c12fbc9725461f830388b7a1ecac77581d7fdf
Dave Berkeley <dave@rotwang.co.uk>
2015-09-01 17:51:37 +0100

    added paho mqtt library

--- MqttControls/app/src/main/java/uk/co/rotwang/mqttcontrols/MainActivity.java
+++ MqttControls/app/src/main/java/uk/co/rotwang/mqttcontrols/MainActivity.java
@@ -2,18 +2,72 @@ package uk.co.rotwang.mqttcontrols;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+class exampleCallBack implements MqttCallback
+{
+    public void connectionLost(Throwable cause)
+    {
+        Log.d(getClass().getCanonicalName(), "MQTT Server connection lost");
+    }
+    public void messageArrived(String topic, MqttMessage message)
+    {
+        Log.d(getClass().getCanonicalName(), "Message arrived:" + topic + ":" + message.toString());
+    }
+    public void deliveryComplete(IMqttDeliveryToken token)
+    {
+        Log.d(getClass().getCanonicalName(), "Delivery complete");
+    }
+};
 
 public class MainActivity extends ActionBarActivity {
+
+    MqttClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MqttSettings conf = new MqttSettings();
+        conf.read(this);
+
+        try {
+            client = new MqttClient(conf.getUrl(), MqttClient.generateClientId(), null);
+            client.setCallback(new exampleCallBack());
+        }
+        catch (MqttException ex) {
+            ex.printStackTrace();
+        }
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        try
+        {
+            client.connect(options);
+        }
+        catch (MqttException e) {
+            Log.d(getClass().getCanonicalName(), "Connection attempt failed with reason code = " + e.getReasonCode() + ":" + e.getCause());
+        }
+
+        try
+        {
+            client.subscribe("home/power");
+        }
+        catch (MqttException e)
+        {
+            Log.d(getClass().getCanonicalName(), "Subscribe failed with reason code = " + e.getReasonCode());
+        }
     }
 
     @Override
@@ -45,3 +99,5 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 }
+
+// FIN
--- MqttControls/app/src/main/java/uk/co/rotwang/mqttcontrols/MqttSettingsActivity.java
+++ MqttControls/app/src/main/java/uk/co/rotwang/mqttcontrols/MqttSettingsActivity.java
@@ -11,34 +11,7 @@ import android.content.SharedPreferences;
 import android.widget.EditText;
 import android.widget.Toast;
 
-    /*
-     *  Data store for settings
-     */
 
-class MqttSettings {
-
-    final String PREFS_NAME = "mqtt_settings";
-
-    String server;
-    int port;
-    String url;
-
-    public void read(Context context) {
-        SharedPreferences sp = context.getSharedPreferences(PREFS_NAME, Context.MODE_WORLD_READABLE);
-        server = sp.getString("server", "");
-        port = sp.getInt("port", 1883);
-        url = sp.getString("url", "");
-    }
-
-    public void save(Context context) {
-        SharedPreferences sp = context.getSharedPreferences(PREFS_NAME, Context.MODE_WORLD_READABLE);
-        SharedPreferences.Editor edit = sp.edit();
-        edit.putString("server", server);
-        edit.putInt("port", port);
-        edit.putString("url", url);
-        edit.commit();
-    }
-};
 
     /*
     *   Activity
